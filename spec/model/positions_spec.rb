166c29a79267118ec60cc769f0d22e95a5911ddd
Eduardo S Marques <edhana@gmail.com>
2011-11-28 19:09:46 -0200

    A valid position now could be saved

--- spec/model/positions_spec.rb
+++ spec/model/positions_spec.rb
@@ -1,4 +1,20 @@
 require ::File.expand_path('../../spec_helper',  __FILE__)
 
-describe Position do
+describe "Position" do
+  it "should not save an invalid position" do 
+    position = Position.new
+    position.save.should be_false
+  end
+
+  it "should save a valid position" do
+    position = Position.new
+    position.vehicle_id = 1
+    position.event_id = 1
+    position.latitude = "12"
+    position.longitude = "14"
+    position.altitude = 1440
+    position.acquired_date = Time.now
+    position.speed = 314
+    position.save.should be_true
+  end
 end
--- src/models/positions.rb
+++ src/models/positions.rb
@@ -8,7 +8,14 @@ class Position
   field :event_id, type: Integer
   field :latitude, type: String
   field :longitude, type: String
+  field :altitude, type: Integer
+  field :acquired_date, type: DateTime
   field :speed, type: Integer
 
-  # validates_presence_of :vehicle_id
+  validates_presence_of :vehicle_id
+  validates_presence_of :event_id
+  validates_presence_of :latitude
+  validates_presence_of :longitude
+  validates_presence_of :speed
+  validates_presence_of :acquired_date
 end
